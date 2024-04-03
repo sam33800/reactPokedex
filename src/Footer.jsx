d3b3834161e8452b8ab292fec460c1c623854533
sam33800 <159770850+sam33800@users.noreply.github.com>
2024-04-03 17:02:18 +0200

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer =()=>{
+    return(
+        <footer>
+            <p>Pokemon Company&copy;</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = ()=>{
+    return(
+        <header>
+            <img src="/images-removebg-preview.png" alt="" />
+        </header>
+
+    )
+}
+
+export default Header;
